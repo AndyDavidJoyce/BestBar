8d068ed8afd15622e3cc584691322c306a6fefb2
Andrew Joyce <andy@zeta.net>
2017-06-05 17:31:11 +0100

    [Add]: Initial Commit

---
+++ app/src/main/java/com/andy/bestbar/ui/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.andy.bestbar.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.andy.bestbar.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
